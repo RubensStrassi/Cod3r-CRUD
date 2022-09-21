09b33fe29dc1ea6b96bf60fe8c213e461ea07b46
RubensStrassi <rubensstrassi@gmail.com>
2022-09-21 09:28:46 -0300

    Navegando para tela de produtos create

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductCreatComponent } from './components/product/product-creat/product-creat.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductCrudComponent
+  },
+  {
+    path: 'products/creat',
+    component: ProductCreatComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,13 +13,19 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/tamplate/header/header.component';
 import { FooterComponent } from './components/tamplate/footer/footer.component';
 import { NavComponent } from './components/tamplate/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreatComponent } from './components/product/product-creat/product-creat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    ProductCreatComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +34,8 @@ import { NavComponent } from './components/tamplate/nav/nav.component';
     MatToolbarModule,
     MatListModule,
     MatSidenavModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
